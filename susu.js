31a37fc5b9be8ca8e190ce6b0549c450ea1679d1
faisalnoufal <faisalnoufal@gmail.com>
2022-12-11 08:50:52 +0530

    fb update

--- susu.js
+++ susu.js
@@ -40,4 +40,18 @@ function webadder() {
 	};
 }
 
+<script>
+window.fbAsyncInit = function() {
+  FB.init({
+    xfbml            : true,
+    version          : 'v15.0'
+  });
+};
 
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
